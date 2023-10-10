adb77da3d6685be1b061e3cef42fc9945780ce9f
Nikita Kuchur <nikitakuchur@gmail.com>
2023-10-11 00:26:21 +0200

    fixed highlighting

--- src/main/java/net/englab/contextsearcher/utils/SubtitleHighlighter.java
+++ src/main/java/net/englab/contextsearcher/utils/SubtitleHighlighter.java
@@ -20,7 +20,7 @@ public class SubtitleHighlighter {
      */
     public static void highlight(String sentence, String[] parts, List<SubtitleBlock> blocks) {
         String originalText = blocks.stream()
-                .map(block -> block.getText().get(0))
+                .map(block -> block.getText().get(0).trim())
                 .filter(text -> !text.isBlank())
                 .collect(Collectors.joining(" "));
 
