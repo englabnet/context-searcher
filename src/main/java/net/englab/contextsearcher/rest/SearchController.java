b03684ba90724af75d425e91c9da5d9a07556e2d
Nikita Kuchur <nikitakuchur@gmail.com>
2024-01-16 21:14:17 +0100

    updated the search endpoint

--- src/main/java/net/englab/contextsearcher/rest/SearchController.java
+++ src/main/java/net/englab/contextsearcher/rest/SearchController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import net.englab.contextsearcher.models.common.EnglishVariety;
 import net.englab.contextsearcher.models.search.VideoFragmentPage;
 import net.englab.contextsearcher.services.VideoSearcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * A REST controller that provides the search functionality.
@@ -34,6 +36,9 @@ public class SearchController {
             EnglishVariety variety,
             @RequestParam(defaultValue = "0") int from, // TODO: change it to a page
             @RequestParam(defaultValue = "10") int size) {
+        if (size >= 50) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The size cannot be more than 50");
+        }
         return videoSearcher.search(phrase, variety, from, size);
     }
 }
